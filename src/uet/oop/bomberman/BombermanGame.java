f77b601fc51225603930d451c72ff2f634d42429
namdnhn <donhuhoangnam.work@gmail.com>
2022-10-13 12:17:49 +0700

    moving commit

--- src/uet/oop/bomberman/BombermanGame.java
+++ src/uet/oop/bomberman/BombermanGame.java
@@ -2,10 +2,19 @@ package uet.oop.bomberman;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.stage.Stage;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.*;
+import javafx.scene.image.*;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import uet.oop.bomberman.entities.Bomber;
 import uet.oop.bomberman.entities.Entity;
@@ -13,6 +22,8 @@ import uet.oop.bomberman.entities.Grass;
 import uet.oop.bomberman.entities.Wall;
 import uet.oop.bomberman.graphics.Sprite;
 
+import javax.security.auth.kerberos.KerberosTicket;
+//import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,6 +34,7 @@ public class BombermanGame extends Application {
     
     private GraphicsContext gc;
     private Canvas canvas;
+    Scene scene;
     private List<Entity> entities = new ArrayList<>();
     private List<Entity> stillObjects = new ArrayList<>();
 
@@ -42,7 +54,7 @@ public class BombermanGame extends Application {
         root.getChildren().add(canvas);
 
         // Tao scene
-        Scene scene = new Scene(root);
+        scene = new Scene(root);
 
         // Them scene vao stage
         stage.setScene(scene);
@@ -59,9 +71,46 @@ public class BombermanGame extends Application {
 
         createMap();
 
-        Entity bomberman = new Bomber(1, 1, Sprite.player_right.getFxImage());
+        Bomber bomberman = new Bomber(1, 1, Sprite.player_right.getFxImage());
         entities.add(bomberman);
+
+        /** Moving Key. */
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case UP:
+                        bomberman.setGoUp(true);
+                        break;
+                    case DOWN:
+                        bomberman.setGoDown(true);
+                        break;
+                    case LEFT:
+                        bomberman.setGoLeft(true);
+                        break;
+                    case RIGHT:
+                        bomberman.setGoRight(true);
+                        break;
+                }
+            }
+        });
+
+        scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case UP:    bomberman.setGoUp(false); break;
+                    case DOWN:  bomberman.setGoDown(false); break;
+                    case LEFT:  bomberman.setGoLeft(false); break;
+                    case RIGHT: bomberman.setGoRight(false); break;
+                }
+            }
+        });
+
+
     }
+
+
 
     public void createMap() {
         for (int i = 0; i < WIDTH; i++) {
@@ -78,6 +127,8 @@ public class BombermanGame extends Application {
         }
     }
 
+
+
     public void update() {
         entities.forEach(Entity::update);
     }
@@ -87,4 +138,8 @@ public class BombermanGame extends Application {
         stillObjects.forEach(g -> g.render(gc));
         entities.forEach(g -> g.render(gc));
     }
+
+
+
+
 }
--- src/uet/oop/bomberman/entities/Bomber.java
+++ src/uet/oop/bomberman/entities/Bomber.java
@@ -1,12 +1,33 @@
 package uet.oop.bomberman.entities;
 
+import javafx.event.Event;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 
 public class Bomber extends Entity {
+    private static double SPEED = 2;
+
+    public void setGoUp(boolean goUp) {
+        this.goUp = goUp;
+    }
+
+    public void setGoDown(boolean goDown) {
+        this.goDown = goDown;
+    }
+
+    public void setGoLeft(boolean goLeft) {
+        this.goLeft = goLeft;
+    }
+
+    public void setGoRight(boolean goRight) {
+        this.goRight = goRight;
+    }
+
+    private boolean goUp, goDown, goLeft, goRight;
 
     public Bomber(int x, int y, Image img) {
         super( x, y, img);
@@ -14,6 +35,22 @@ public class Bomber extends Entity {
 
     @Override
     public void update() {
+        if (goUp) {
+            this.y -= SPEED;
+        }
 
+        if (goDown) {
+            this.y += SPEED;
+        }
+
+        if (goLeft) {
+            this.x -= SPEED;
+        }
+
+        if (goRight) {
+            this.x += SPEED;
+        }
     }
+
+
 }
